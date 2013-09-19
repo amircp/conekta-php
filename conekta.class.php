c8a5d2d7d80e17887f5d07a7d829d55cb409229d
Amir Canto <cantoamir@gmail.com>
2013-09-19 10:06:10 -0500

    Conekta Class created

---
+++ conekta.class.php
@@ -0,0 +1,67 @@
+<?php
+/*************************************************
+ *	
+ *	@author: Amir Canto / http://www.twitter.com/amircp
+ *	@date: September 19, 2013.
+ *	@version: 1.0
+ *	@class: 	Conekta
+ *	@description: Conekta Class, get payments for your website! first version, very basic.
+ *
+**/
+
+
+class Conekta
+{
+
+	private $_apiKey = '';
+
+	function __construct($key='')
+	{
+
+		$this->_apiKey = $key;  // Si no queremos usar setAccessToken...
+	}
+
+
+	public function setAccessToken($key)
+	{
+		$this->_apiKey = $key;
+	}
+	public function requestData($postData)
+	{
+		if(empty($this->_apiKey)) return 0;
+		// Cabeceras HTTP extras para Conekta:
+		$headers = array('Accept: application/vnd.conekta-v0.2.0+json',
+						 'Content-type: application/json'
+					);
+
+		/*foreach($postData as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		$postData= substr($fields_string,0,strlen($fields_string) -1 );
+		*/
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_USERPWD, $this->_apiKey.":");
+		curl_setopt($ch,CURLOPT_URL, "https://api.conekta.io/charges");			 // URL API 
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  // Cabeceras API
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // No verificamos certificado SSL (no body cares).
+		curl_setopt($ch, CURLOPT_POST, 1);				 // Peticiones POST
+ 		curl_setopt($ch, CURLOPT_POSTFIELDS,$postData);	 // Mandamos el Json
+ 		curl_setopt($ch, CURLOPT_HEADER,0);  			 //Retornar cabeceras 
+ 		curl_setopt($ch, CURLOPT_RETURNTRANSFER  ,1);    //Retornar datos de llamada
+		$respuesta = curl_exec($ch);					
+		return $respuesta;
+	}
+
+	public function makePayment($data)
+	{
+		if(is_array($data))
+		{	
+			$data = json_encode($data);
+			$response = $this->requestData($data);
+			return json_decode($response);
+		}
+	}
+	
+
+}
+
+
+?>
---
+++ foo.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Using Conekta Class
+*/
+require_once("conekta.class.php");
+
+$key = ''
+$Conekta = new Conekta($key); // Creando el objeto Conekta
+// binding request 
+/* OXXO Request */
+$request = array(
+					'amount' => 90000,
+     				'currency' => 'MXN',
+     				'description' => 'DVD - Zorro',
+     				'customer'=> array('email' => 'gil.gil@mypayments.mx'),
+     				'cash'=> array('type' => 'oxxo')
+	);
+$response = $Conekta->makePayment($request);
+
+print_r($response);
+
+
+/* Bank  Request*/
+$request = array(
+
+ 	'amount' => 100000,
+	'currency' => 'MXN',
+	'description' => 'LOL Article',
+	'customer' => array(
+    						'name' => 'Amir Canto',
+ 	  		 				'email' => 'amir@hotmail.com',
+    						'phone' => '555555550'
+						),
+  	'bank' => array('type' => 'banorte')
+);
+$response = $Conekta->makePayment($request);
+print_r($response);
+
+
+
+
+/* Card Request*/
+$request = array
+(
+ 	'amount' => 100000,
+	'currency' => 'MXN',
+	'description' => 'LOL Article',
+	'customer' => array(
+    						'name' => 'Amir Canto',
+ 	  		 				'email' => 'amir@hotmail.com',
+    						'phone' => '555555550'
+						),
+
+	 'card' => array(
+     					'number' => '4111111111111111',
+      					'exp_month' => '01',
+    					'exp_year' => '17',
+    					'cvc' => '000',
+    					'name' => 'Mario Moreno Cantinflas',
+ 					)
+);
+$response = $Conekta->makePayment($request);
+print_r($response);
+
+
+?>
